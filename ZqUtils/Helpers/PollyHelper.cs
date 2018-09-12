94eb3c5301aad19054851ff9d2169e86e46a2b65
zqlovejyc <943620963@qq.com>
2018-09-12 16:21:22 +0800

    Polly工具类新增异步方法

--- ZqUtils/Helpers/PollyHelper.cs
+++ ZqUtils/Helpers/PollyHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Polly;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using ZqUtils.Extensions;
 /****************************
 * [Author] 张强
@@ -15,6 +16,7 @@ namespace ZqUtils.Helpers
     public class PollyHelper
     {
         #region WaitAndRetry    
+        #region Sync
         /// <summary>
         /// Polly重试指定次数
         /// </summary>
@@ -93,7 +95,88 @@ namespace ZqUtils.Helpers
         }
         #endregion
 
+        #region Async
+        /// <summary>
+        /// Polly重试指定次数
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="actionException"></param>
+        /// <param name="sleepDurations"></param>
+        /// <param name="onRetry"></param>
+        public static async void WaitAndRetryAsync<T>(Func<Task> action, Action<Exception> actionException, IEnumerable<TimeSpan> sleepDurations, Action<Exception, TimeSpan, int, Context> onRetry) where T : Exception
+        {
+            try
+            {
+                //异常重试事件
+                if (onRetry == null)
+                {
+                    void OnRetry(Exception exception, TimeSpan timeSpan, int count, Context context) => LogHelper.Error(exception, $"异常：{exception.Message}，时间：{timeSpan}，重试次数：{count}，内容：{context.ToJson()}");
+                    onRetry = OnRetry;
+                }
+                await Policy
+                        .Handle<T>()
+                        .WaitAndRetryAsync(sleepDurations, onRetry)
+                        .ExecuteAsync(action);
+            }
+            catch (Exception ex)
+            {
+                if (actionException != null)
+                {
+                    actionException(ex);
+                }
+                else
+                {
+                    LogHelper.Error(ex, "WaitAndRetry");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Polly重试指定次数
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="retryCount"></param>
+        /// <param name="actionException"></param>
+        /// <param name="sleepDurationProvider"></param>
+        /// <param name="onRetry"></param>
+        public static async void WaitAndRetryAsync<T>(Func<Task> action, int retryCount, Action<Exception> actionException, Func<int, TimeSpan> sleepDurationProvider, Action<Exception, TimeSpan, int, Context> onRetry) where T : Exception
+        {
+            try
+            {
+                //延迟机制
+                if (sleepDurationProvider == null)
+                {
+                    TimeSpan SleepDurationProvider(int retryAttempt) => TimeSpan.FromSeconds(Math.Pow(3, retryAttempt));
+                    sleepDurationProvider = SleepDurationProvider;
+                }
+                //异常重试事件
+                if (onRetry == null)
+                {
+                    void OnRetry(Exception exception, TimeSpan timeSpan, int count, Context context) => LogHelper.Error(exception, $"异常：{exception.Message}，时间：{timeSpan}，重试次数：{count}，内容：{context.ToJson()}");
+                    onRetry = OnRetry;
+                }
+                await Policy
+                        .Handle<T>()
+                        .WaitAndRetryAsync(retryCount, sleepDurationProvider, onRetry)
+                        .ExecuteAsync(action);
+            }
+            catch (Exception ex)
+            {
+                if (actionException != null)
+                {
+                    actionException(ex);
+                }
+                else
+                {
+                    LogHelper.Error(ex, "WaitAndRetry");
+                }
+            }
+        }
+        #endregion
+        #endregion
+
         #region WaitAndRetryForever
+        #region Sync
         /// <summary>
         /// Polly永久重试机制
         /// </summary>
@@ -147,6 +230,67 @@ namespace ZqUtils.Helpers
                .WaitAndRetryForever(sleepDurationProvider, onRetry)
                .Execute(action);
         }
+        #endregion
+
+        #region Async
+        /// <summary>
+        /// Polly永久重试机制
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="action"></param>
+        /// <param name="sleepDurationProvider"></param>
+        /// <param name="onRetry"></param>
+        public static async void WaitAndRetryForeverAsync<T>(Func<Task> action, Func<int, TimeSpan> sleepDurationProvider, Action<Exception, TimeSpan> onRetry) where T : Exception
+        {
+            //延迟机制
+            if (sleepDurationProvider == null)
+            {
+                TimeSpan SleepDurationProvider(int retryAttempt) => TimeSpan.FromSeconds(Math.Pow(3, retryAttempt));
+                sleepDurationProvider = SleepDurationProvider;
+            }
+            //异常重试事件
+            if (onRetry == null)
+            {
+                void OnRetry(Exception exception, TimeSpan timeSpan) => LogHelper.Error(exception, $"异常：{exception.Message}，时间：{timeSpan}");
+                onRetry = OnRetry;
+            }
+            await Policy
+                    .Handle<T>()
+                    .WaitAndRetryForeverAsync(sleepDurationProvider, onRetry)
+                    .ExecuteAsync(action);
+        }
+
+        /// <summary>
+        /// Polly永久重试机制
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="action"></param>
+        /// <param name="sleepDurationProvider"></param>
+        /// <param name="onRetryAsync"></param>
+        public static async void WaitAndRetryForeverAsync<T>(Func<Task> action, Func<int, Exception, Context, TimeSpan> sleepDurationProvider, Func<Exception, TimeSpan, Context, Task> onRetryAsync) where T : Exception
+        {
+            //延迟机制
+            if (sleepDurationProvider == null)
+            {
+                TimeSpan SleepDurationProvider(int retryAttempt, Exception exception, Context context) => TimeSpan.FromSeconds(Math.Pow(3, retryAttempt));
+                sleepDurationProvider = SleepDurationProvider;
+            }
+            //异常重试事件
+            if (onRetryAsync == null)
+            {
+                Task OnRetryAsync(Exception exception, TimeSpan timeSpan, Context context)
+                {
+                    LogHelper.Error(exception, $"异常：{exception.Message}，时间：{timeSpan}，内容：{context.ToJson()}");
+                    return Task.FromResult(0);
+                }
+                onRetryAsync = OnRetryAsync;
+            }
+            await Policy
+                    .Handle<T>()
+                    .WaitAndRetryForeverAsync(sleepDurationProvider, onRetryAsync)
+                    .ExecuteAsync(action);
+        }
+        #endregion
         #endregion
     }
 }
