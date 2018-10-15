32aedb066d0bef793c336929e6bb382bcd9db7b9
zqlovejyc <943620963@qq.com>
2018-10-15 15:16:46 +0800

    更新GpsHelper工具类；

--- ZqUtils/Helpers/GpsHelper.cs
+++ ZqUtils/Helpers/GpsHelper.cs
@@ -13,52 +13,45 @@ namespace ZqUtils.Helpers
     public class GpsHelper
     {
         #region 测距-方法1
-
-        //以下算法参考：https://blog.csdn.net/xiejm2333/article/details/73297004
-
         /// <summary>
         /// 计算两点GPS坐标的距离（单位：米）
+        /// 参考：https://blog.csdn.net/xiejm2333/article/details/73297004
         /// </summary>
-        /// <param name="n1">第一点的纬度坐标</param>
-        /// <param name="e1">第一点的经度坐标</param>
-        /// <param name="n2">第二点的纬度坐标</param>
-        /// <param name="e2">第二点的经度坐标</param>
-        /// <returns></returns>
-        public static double Distance(double n1, double e1, double n2, double e2)
+        /// <param name="lat1">纬度1</param>
+        /// <param name="lng1">经度1</param>
+        /// <param name="lat2">纬度2</param>
+        /// <param name="lng2">经度2</param>
+        /// <param name="earthRadius">地球半径，默认：6378137.0米</param>
+        /// <returns>距离(米)</returns>
+        public static double Distance(double lat1, double lng1, double lat2, double lng2, double earthRadius = 6378137.0)
         {
             double Rad(double d)
             {
                 return d * Math.PI / 180.00; //角度转换成弧度
             }
-            double Lat1 = Rad(n1); // 纬度
-            double Lat2 = Rad(n2);
+            double Lat1 = Rad(lat1); // 纬度
+            double Lat2 = Rad(lat2);
             double a = Lat1 - Lat2;//两点纬度之差
-            double b = Rad(e1) - Rad(e2); //经度之差
+            double b = Rad(lng1) - Rad(lng2); //经度之差
             double s = 2 * Math.Asin(Math.Sqrt(Math.Pow(Math.Sin(a / 2), 2) + Math.Cos(Lat1) * Math.Cos(Lat2) * Math.Pow(Math.Sin(b / 2), 2)));//计算两点距离的公式
-            s = s * 6378137.0;//弧长乘地球半径（半径为米）
+            s = s * earthRadius;//弧长乘地球半径（半径为米）
             s = Math.Round(s * 10000d) / 10000d;//精确距离的数值
             return s;
         }
         #endregion
 
         #region 测距-方法2
-
-        //参考：https://blog.csdn.net/xiejm2333/article/details/73297004
-
         /// <summary>
-        /// 百度百科： 6371.393 km ：https://baike.baidu.com/item/%E5%9C%B0%E7%90%83%E5%8D%8A%E5%BE%84/1037801?fr=aladdin
+        /// 计算两点GPS坐标的距离（单位：米）
+        /// 参考：https://www.cnblogs.com/softfair/p/distance_of_two_latitude_and_longitude_points.html
         /// </summary>
-        public static double EARTH_RADIUS = 6371393.0;//m 地球半径 平均值，千米 / 也有数据：6378137.0米
-
-        /// <summary>
-        /// 给定的经度1，纬度1；经度2，纬度2. 计算2个经纬度之间的距离。
-        /// </summary>
-        /// <param name="lat1">经度1</param>
-        /// <param name="lon1">纬度1</param>
-        /// <param name="lat2">经度2</param>
-        /// <param name="lon2">纬度2</param>
-        /// <returns>距离（公里、千米）</returns>
-        public static double Distance2(double lat1, double lon1, double lat2, double lon2)
+        /// <param name="lat1">纬度1</param>
+        /// <param name="lng1">经度1</param>
+        /// <param name="lat2">纬度2</param>
+        /// <param name="lng2">经度2</param>
+        /// <param name="earthRadius">地球半径，默认：6371393.0米</param>
+        /// <returns>距离(米)</returns>
+        public static double Distance2(double lat1, double lng1, double lat2, double lng2, double earthRadius = 6371393.0)
         {
             double HaverSin(double theta)
             {
@@ -74,20 +67,20 @@ namespace ZqUtils.Helpers
             //用haversine公式计算球面两点间的距离。
             //经纬度转换成弧度
             lat1 = ConvertDegreesToRadians(lat1);
-            lon1 = ConvertDegreesToRadians(lon1);
+            lng1 = ConvertDegreesToRadians(lng1);
             lat2 = ConvertDegreesToRadians(lat2);
-            lon2 = ConvertDegreesToRadians(lon2);
+            lng2 = ConvertDegreesToRadians(lng2);
 
             //差值
-            var vLon = Math.Abs(lon1 - lon2);
+            var vLon = Math.Abs(lng1 - lng2);
             var vLat = Math.Abs(lat1 - lat2);
 
             //h is the great circle distance in radians, great circle就是一个球体上的切面，它的圆心即是球心的一个周长最大的圆。
             var h = HaverSin(vLat) + Math.Cos(lat1) * Math.Cos(lat2) * HaverSin(vLon);
 
-            var distance = 2 * EARTH_RADIUS * Math.Asin(Math.Sqrt(h));
+            var distance = 2 * earthRadius * Math.Asin(Math.Sqrt(h));
 
-            return distance;
+            return distance * 1000;
         }
         #endregion
 
@@ -98,11 +91,12 @@ namespace ZqUtils.Helpers
         /// <param name="lat">纬度</param>
         /// <param name="lng">经度</param>
         /// <param name="distance">偏移距离</param>
+        /// <param name="earthRadius">地球半径，默认：6371393.0米</param>
         /// <returns>经纬度范围</returns>
-        public static (double minLat, double maxLat, double minLng, double maxLng) GetLatAndLngRange(double lat, double lng, double distance)
+        public static (double minLat, double maxLat, double minLng, double maxLng) GetLatAndLngRange(double lat, double lng, double distance, double earthRadius = 6371393.0)
         {
             double dis = distance / 1000;
-            double earthR = EARTH_RADIUS / 1000;//地球半径千米
+            double earthR = earthRadius / 1000;//地球半径千米
 
             double dlng = 2 * Math.Asin(Math.Sin(dis / (2 * earthR)) / Math.Cos(lat * Math.PI / 180));//定义三角函数的输入和输出都采用弧度值
             dlng = dlng * 180 / Math.PI;//角度转为弧度  
