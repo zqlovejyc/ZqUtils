28630bde875228e94931de57daf2a6bfcb5623d7
zqlovejyc <943620963@qq.com>
2019-07-25 18:16:37 +0800

    新增RSA密钥转换工具类

---
+++ ZqUtils/Helpers/RSAKeyConvertHelper.cs
@@ -0,0 +1,110 @@
+﻿#region License
+/***
+ * Copyright © 2018-2019, 张强 (943620963@qq.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * without warranties or conditions of any kind, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#endregion
+
+using System;
+using System.Xml;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
+/****************************
+ * [Author] 张强
+ * [Date] 2019-07-25
+ * [Describe] RSA密钥转换工具类
+ * **************************/
+namespace ZqUtils.Helpers
+{
+    /// <summary>
+    /// RSA密钥转换工具类
+    /// </summary>
+    public class RSAKeyConvertHelper
+    {
+        /// <summary>    
+        /// RSA私钥格式转换，java->.net    
+        /// </summary>    
+        /// <param name="privateKey">java生成的RSA私钥</param>    
+        /// <returns></returns>    
+        public static string RSAPrivateKeyJava2DotNet(string privateKey)
+        {
+            var privateKeyParam = (RsaPrivateCrtKeyParameters)PrivateKeyFactory.CreateKey(Convert.FromBase64String(privateKey));
+            return string.Format("<RSAKeyValue><Modulus>{0}</Modulus><Exponent>{1}</Exponent><P>{2}</P><Q>{3}</Q><DP>{4}</DP><DQ>{5}</DQ><InverseQ>{6}</InverseQ><D>{7}</D></RSAKeyValue>",
+                Convert.ToBase64String(privateKeyParam.Modulus.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.PublicExponent.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.P.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.Q.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.DP.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.DQ.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.QInv.ToByteArrayUnsigned()),
+                Convert.ToBase64String(privateKeyParam.Exponent.ToByteArrayUnsigned()));
+        }
+
+        /// <summary>    
+        /// RSA私钥格式转换，.net->java    
+        /// </summary>    
+        /// <param name="privateKey">.net生成的私钥</param>    
+        /// <returns></returns>    
+        public static string RSAPrivateKeyDotNet2Java(string privateKey)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(privateKey);
+            var m = new BigInteger(1, Convert.FromBase64String(doc.DocumentElement.GetElementsByTagName("Modulus")[0].InnerText));
+            var exp = new BigInteger(1, Convert.FromBase64String(doc.DocumentElement.GetElementsByTagName("Exponent")[0].InnerText));
+            var d = new BigInteger(1, Convert.FromBase64String(doc.DocumentElement.GetElementsByTagName("D")[0].InnerText));
+            var p = new BigInteger(1, Convert.FromBase64String(doc.DocumentElement.GetElementsByTagName("P")[0].InnerText));
+            var q = new BigInteger(1, Convert.FromBase64String(doc.DocumentElement.GetElementsByTagName("Q")[0].InnerText));
+            var dp = new BigInteger(1, Convert.FromBase64String(doc.DocumentElement.GetElementsByTagName("DP")[0].InnerText));
+            var dq = new BigInteger(1, Convert.FromBase64String(doc.DocumentElement.GetElementsByTagName("DQ")[0].InnerText));
+            var qinv = new BigInteger(1, Convert.FromBase64String(doc.DocumentElement.GetElementsByTagName("InverseQ")[0].InnerText));
+            var privateKeyParam = new RsaPrivateCrtKeyParameters(m, exp, d, p, q, dp, dq, qinv);
+            var privateKeyInfo = PrivateKeyInfoFactory.CreatePrivateKeyInfo(privateKeyParam);
+            var serializedPrivateBytes = privateKeyInfo.ToAsn1Object().GetEncoded();
+            return Convert.ToBase64String(serializedPrivateBytes);
+        }
+
+        /// <summary>    
+        /// RSA公钥格式转换，java->.net    
+        /// </summary>    
+        /// <param name="publicKey">java生成的公钥</param>    
+        /// <returns></returns>    
+        public static string RSAPublicKeyJava2DotNet(string publicKey)
+        {
+            var publicKeyParam = (RsaKeyParameters)PublicKeyFactory.CreateKey(Convert.FromBase64String(publicKey));
+            return string.Format("<RSAKeyValue><Modulus>{0}</Modulus><Exponent>{1}</Exponent></RSAKeyValue>",
+                Convert.ToBase64String(publicKeyParam.Modulus.ToByteArrayUnsigned()),
+                Convert.ToBase64String(publicKeyParam.Exponent.ToByteArrayUnsigned()));
+        }
+
+        /// <summary>    
+        /// RSA公钥格式转换，.net->java    
+        /// </summary>    
+        /// <param name="publicKey">.net生成的公钥</param>    
+        /// <returns></returns>    
+        public static string RSAPublicKeyDotNet2Java(string publicKey)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(publicKey);
+            var m = new BigInteger(1, Convert.FromBase64String(doc.DocumentElement.GetElementsByTagName("Modulus")[0].InnerText));
+            var p = new BigInteger(1, Convert.FromBase64String(doc.DocumentElement.GetElementsByTagName("Exponent")[0].InnerText));
+            var pub = new RsaKeyParameters(false, m, p);
+            var publicKeyInfo = SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(pub);
+            var serializedPublicBytes = publicKeyInfo.ToAsn1Object().GetDerEncoded();
+            return Convert.ToBase64String(serializedPublicBytes);
+        }
+    }
+}
