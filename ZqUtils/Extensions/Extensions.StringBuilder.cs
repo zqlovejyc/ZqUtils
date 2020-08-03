54000131f11ec7afe5eec2f0cb35678cc241cd12
zqlovejyc <943620963@qq.com>
2020-08-03 15:12:04 +0800

    重载AppendIf扩展方法；

--- ZqUtils/Extensions/Extensions.StringBuilder.cs
+++ ZqUtils/Extensions/Extensions.StringBuilder.cs
@@ -1600,6 +1600,30 @@ namespace ZqUtils.Extensions
 
             return @this;
         }
+
+        /// <summary>
+        /// A StringBuilder extension method that appends a when.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <param name="callback">The callback delegate when the predicate is true.</param>
+        /// <param name="values">A variable-length parameters list containing values.</param>
+        /// <returns>A StringBuilder.</returns>
+        public static StringBuilder AppendIf<T>(this StringBuilder @this, Func<T, bool> predicate, Action callback, params T[] values)
+        {
+            foreach (var value in values)
+            {
+                if (predicate(value))
+                {
+                    @this.Append(value);
+
+                    callback?.Invoke();
+                }
+            }
+
+            return @this;
+        }
         #endregion
 
         #region AppendJoin
