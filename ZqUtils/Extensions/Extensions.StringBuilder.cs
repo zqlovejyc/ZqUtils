a72108ee117dc786dd3069551773694e5a6f6806
zqlovejyc <943620963@qq.com>
2021-10-28 15:14:41 +0800

    重载StringBuilder的AppendIf、AppendLineIf扩展方法；

--- ZqUtils/Extensions/Extensions.StringBuilder.cs
+++ ZqUtils/Extensions/Extensions.StringBuilder.cs
@@ -1677,6 +1677,27 @@ namespace ZqUtils.Extensions
         /// <typeparam name="T">Generic type parameter.</typeparam>
         /// <param name="this">The @this to act on.</param>
         /// <param name="predicate">The predicate.</param>
+        /// <param name="values">A variable-length parameters list containing values.</param>
+        /// <returns>A StringBuilder.</returns>
+        public static StringBuilder AppendIf<T>(this StringBuilder @this, Func<T, bool> predicate, params Func<T>[] values)
+        {
+            foreach (var value in values)
+            {
+                if (predicate(value()))
+                {
+                    @this.Append(value);
+                }
+            }
+
+            return @this;
+        }
+
+        /// <summary>
+        /// A StringBuilder extension method that appends a when.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="predicate">The predicate.</param>
         /// <param name="callback">The callback delegate when the predicate is true.</param>
         /// <param name="values">A variable-length parameters list containing values.</param>
         /// <returns>A StringBuilder.</returns>
@@ -1685,6 +1706,30 @@ namespace ZqUtils.Extensions
             foreach (var value in values)
             {
                 if (predicate(value))
+                {
+                    @this.Append(value);
+
+                    callback?.Invoke();
+                }
+            }
+
+            return @this;
+        }
+
+        /// <summary>
+        /// A StringBuilder extension method that appends a when.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <param name="callback">The callback delegate when the predicate is true.</param>
+        /// <param name="values">A variable-length parameters list containing values.</param>
+        /// <returns>A StringBuilder.</returns>
+        public static StringBuilder AppendIf<T>(this StringBuilder @this, Func<T, bool> predicate, Action callback, params Func<T>[] values)
+        {
+            foreach (var value in values)
+            {
+                if (predicate(value()))
                 {
                     @this.Append(value);
 
@@ -1778,6 +1823,26 @@ namespace ZqUtils.Extensions
         /// <typeparam name="T">Generic type parameter.</typeparam>
         /// <param name="this">The @this to act on.</param>
         /// <param name="predicate">The predicate.</param>
+        /// <param name="values">A variable-length parameters list containing values.</param>
+        /// <returns>A StringBuilder.</returns>
+        public static StringBuilder AppendLineIf<T>(this StringBuilder @this, Func<T, bool> predicate, params Func<T>[] values)
+        {
+            foreach (var value in values)
+            {
+                if (predicate(value()))
+                {
+                    @this.AppendLine(value.ToString());
+                }
+            }
+            return @this;
+        }
+
+        /// <summary>
+        /// A StringBuilder extension method that appends a line when.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="predicate">The predicate.</param>
         /// <param name="callback">The callback delegate when the predicate is true.</param>
         /// <param name="values">A variable-length parameters list containing values.</param>
         /// <returns>A StringBuilder.</returns>
@@ -1786,6 +1851,29 @@ namespace ZqUtils.Extensions
             foreach (var value in values)
             {
                 if (predicate(value))
+                {
+                    @this.AppendLine(value.ToString());
+
+                    callback?.Invoke();
+                }
+            }
+            return @this;
+        }
+
+        /// <summary>
+        /// A StringBuilder extension method that appends a line when.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <param name="callback">The callback delegate when the predicate is true.</param>
+        /// <param name="values">A variable-length parameters list containing values.</param>
+        /// <returns>A StringBuilder.</returns>
+        public static StringBuilder AppendLineIf<T>(this StringBuilder @this, Func<T, bool> predicate, Action callback, params Func<T>[] values)
+        {
+            foreach (var value in values)
+            {
+                if (predicate(value()))
                 {
                     @this.AppendLine(value.ToString());
 
