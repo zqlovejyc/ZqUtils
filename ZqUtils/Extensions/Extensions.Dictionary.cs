c9582b836a1510422d689c960326a37060f3e99e
zqlovejyc <943620963@qq.com>
2022-09-10 12:16:57 +0800

    重载TryGetValue扩展方法；新增TryGetOrAdd扩展方法；

--- ZqUtils/Extensions/Extensions.Dictionary.cs
+++ ZqUtils/Extensions/Extensions.Dictionary.cs
@@ -258,6 +258,29 @@ namespace ZqUtils.Extensions
         /// <summary>
         /// This method is used to try to get a value in a dictionary if it does exists.
         /// </summary>
+        /// <typeparam name="T">Type of the value</typeparam>
+        /// <param name="this">The collection object</param>
+        /// <param name="key">Key</param>
+        /// <param name="ignoreCase">If ignore case the key</param>
+        /// <param name="defaultValue">default value if key not exists</param>
+        /// <returns>The value corresponding to the dictionary key otherwise return the defaultValue</returns>
+        public static T TryGetValue<T>(this IDictionary<string, object> @this, string key, bool ignoreCase, T defaultValue = default)
+        {
+            if (!ignoreCase)
+                return @this.TryGetValue(key, defaultValue);
+
+            if (@this.IsNull() || !@this.Keys.Any(k => k.EqualIgnoreCase(key)))
+                return defaultValue;
+
+            if (@this.FirstOrDefault(o => o.Key.EqualIgnoreCase(key)).Value is T t)
+                return t;
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// This method is used to try to get a value in a dictionary if it does exists.
+        /// </summary>
         /// <typeparam name="TKey">Type of the key</typeparam>
         /// <typeparam name="TValue">Type of the value</typeparam>
         /// <param name="this">The collection object</param>
@@ -270,6 +293,136 @@ namespace ZqUtils.Extensions
                 return defaultValue;
 
             return @this[key];
+        }
+
+        /// <summary>
+        /// This method is used to try to get a value in a dictionary if it does exists.
+        /// </summary>
+        /// <typeparam name="TKey">Type of the key</typeparam>
+        /// <typeparam name="TValue">Type of the value</typeparam>
+        /// <param name="this">The collection object</param>
+        /// <param name="key">Key</param>
+        /// <param name="ignoreCase">If ignore case the key</param>
+        /// <param name="defaultValue">default value if key not exists</param>
+        /// <returns>The value corresponding to the dictionary key otherwise return the defaultValue</returns>
+        public static TValue TryGetValue<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, bool ignoreCase, TValue defaultValue = default)
+        {
+            if (!ignoreCase)
+                return @this.TryGetValue(key, defaultValue);
+
+            bool match(TKey k) =>
+                typeof(TKey) == typeof(string)
+                ? string.Equals(k as string, key as string, StringComparison.OrdinalIgnoreCase)
+                : Equals(k, key);
+
+            if (@this.IsNull() || !@this.Keys.Any(match))
+                return defaultValue;
+
+            return @this.FirstOrDefault(o => match(o.Key)).Value;
+        }
+        #endregion
+
+        #region TryGetOrAdd
+        /// <summary>
+        /// This method is used to try to get a value in a dictionary or add value to dictionary.
+        /// </summary>
+        /// <typeparam name="T">Type of the value</typeparam>
+        /// <param name="this">The collection object</param>
+        /// <param name="key">Key</param>
+        /// <param name="func">The delegate for add</param>
+        /// <returns>The value corresponding to the dictionary key otherwise return the defaultValue</returns>
+        public static T TryGetOrAdd<T>(this IDictionary<string, object> @this, string key, Func<T> func)
+        {
+            if (@this.IsNull() || func.IsNull())
+                return default;
+
+            if (@this.TryGetValue(key, out var val) && val is T retval)
+                return retval;
+
+            @this[key] = retval = func();
+
+            return retval;
+        }
+
+        /// <summary>
+        /// This method is used to try to get a value in a dictionary or add value to dictionary.
+        /// </summary>
+        /// <typeparam name="T">Type of the value</typeparam>
+        /// <param name="this">The collection object</param>
+        /// <param name="key">Key</param>
+        /// <param name="func">The delegate for add</param>
+        /// <param name="ignoreCase">If ignore case the key</param>
+        /// <returns>The value corresponding to the dictionary key otherwise return the defaultValue</returns>
+        public static T TryGetOrAdd<T>(this IDictionary<string, object> @this, string key, Func<T> func, bool ignoreCase)
+        {
+            if (!ignoreCase)
+                return @this.TryGetOrAdd(key, func);
+
+            if (@this.IsNull() || func.IsNull())
+                return default;
+
+            if (!@this.Keys.Any(k => k.EqualIgnoreCase(key)))
+            {
+                var retval = func();
+                @this[key] = retval;
+                return retval;
+            }
+
+            if (@this.FirstOrDefault(o => o.Key.EqualIgnoreCase(key)).Value is T t)
+                return t;
+
+            return default;
+        }
+
+        /// <summary>
+        /// This method is used to try to get a value in a dictionary or add value to dictionary.
+        /// </summary>
+        /// <typeparam name="TKey">Type of the key</typeparam>
+        /// <typeparam name="TValue">Type of the value</typeparam>
+        /// <param name="this">The collection object</param>
+        /// <param name="key">Key</param>
+        /// <param name="func">The delegate for add</param>
+        /// <returns>The value corresponding to the dictionary key otherwise return the defaultValue</returns>
+        public static TValue TryGetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, Func<TValue> func)
+        {
+            if (@this.IsNull() || func.IsNull())
+                return default;
+
+            if (@this.TryGetValue(key, out var val) && val is TValue retval)
+                return retval;
+
+            @this[key] = retval = func();
+
+            return retval;
+        }
+
+        /// <summary>
+        /// This method is used to try to get a value in a dictionary or add value to dictionary.
+        /// </summary>
+        /// <typeparam name="TKey">Type of the key</typeparam>
+        /// <typeparam name="TValue">Type of the value</typeparam>
+        /// <param name="this">The collection object</param>
+        /// <param name="key">Key</param>
+        /// <param name="func">The delegate for add</param>
+        /// <param name="ignoreCase">If ignore case the key</param>
+        /// <returns>The value corresponding to the dictionary key otherwise return the defaultValue</returns>
+        public static TValue TryGetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, Func<TValue> func, bool ignoreCase)
+        {
+            if (!ignoreCase)
+                return @this.TryGetOrAdd(key, func);
+
+            if (@this.IsNull() || func.IsNull())
+                return default;
+
+            bool match(TKey k) =>
+                 typeof(TKey) == typeof(string)
+                 ? string.Equals(k as string, key as string, StringComparison.OrdinalIgnoreCase)
+                 : Equals(k, key);
+
+            if (!@this.Keys.Any(match))
+                return @this[key] = func();
+
+            return @this.FirstOrDefault(o => match(o.Key)).Value;
         }
         #endregion
 
